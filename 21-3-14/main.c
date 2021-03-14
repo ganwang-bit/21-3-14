05af53bc0d0c745f918703f79b89d8b14f56ff3c
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-03-14 21:19:03 +0800

    21-3-14

---
+++ 21-3-14/main.c
@@ -0,0 +1,39 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    float a=7.146;
+//    printf("%.2f",a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    long n,i,j,p=0,ci;
+//    scanf("%ld",&n);
+//    long double arr[n];
+//    long double sum=0;
+//    if(n==0)
+//    {
+//        printf("%.2lf",0);
+//        return 0;
+//    }
+//    for(i=0;i<n;i++)
+//        scanf("%llf",&arr[i]);
+//    for(i=0;i<n;i++)
+//    {
+//        ci=n-i+p-i;
+//        sum+=arr[i]*ci;
+//        p=ci;
+//    }
+//
+//    printf("%.2llf",sum);
+//    return 0;
+//}
